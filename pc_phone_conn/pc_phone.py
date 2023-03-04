01375259cfd20e12a45147b60ea0e85d6348dbc2
Daniel Mokeev <daniel.mokeev1@gmail.com>
2023-03-04 23:14:27 +0100

    working multicontroller

--- pc_phone_conn/pc_phone.py
+++ pc_phone_conn/pc_phone.py
@@ -1,7 +1,8 @@
+import asyncio
 import evdev
-import os
 from enum import Enum
-def get_devices(start=0):
+
+def get_devices():
     devs = []
     for path in evdev.list_devices():
         device = evdev.InputDevice(path)
@@ -22,20 +23,27 @@ class Action(Enum):
     BREAK = 3
     PAUSE = 4
 
+rot = (8600,24200)
 class TurnState:
-    def __init__(self, value, tol=0.05) -> None:
+    def __init__(self, value, tol=0.01) -> None:
         self.value = value
         self.action = Action.LEFT
         self.tol = tol
-    def update(self, value):
-        if abs(value - self.value)/self.value < self.tol: return
+    def update(self, value) -> float:
+        if abs(value - self.value)/self.value < self.tol: return 0
         if value > self.value:
             self.action = Action.RIGHT
         else:
             self.action = Action.LEFT
+        num = ((180) / (rot[-1] - rot[0]))
+        value_deg =  num * (value - rot[0])
+        curr_value_deg = num * (self.value - rot[0])
         self.value = value
+        return value_deg - curr_value_deg
+        
+
 class AccelerateState:
-    def __init__(self, value, tol=0.05) -> None:
+    def __init__(self, value, tol=0.01) -> None:
         self.value = value
         self.action = Action.GAS
         self.tol = tol
@@ -48,13 +56,10 @@ class AccelerateState:
             self.action = Action.GAS
         self.value = value
 
-if __name__ == "__main__":
-    devs = get_devices()
-    for dev in devs:
-        print(dev.capabilities())
-        turn = TurnState(1)
-        acc = AccelerateState(1)
-        for event in dev.read_loop():
+
+async def print_events(device, id, turn, acc):
+    # turn, acc = obj[device]
+    async for event in device.async_read_loop():
             code = event.code
             type = event.type
             value = event.value
@@ -62,11 +67,42 @@ if __name__ == "__main__":
             if type_code in event_types:
                 event_type = event_types[type_code]
                 if event_type == "ABS_WHEEL":
-                    print(value)
-                    turn.update(value)
+                    deg = turn.update(value)
+                    print(id, turn.action, acc.action, deg)
                 elif event_type == "ABS_GAS":
                     acc.update(value)
-                print(turn.action, acc.action)
+                    print(id, turn.action, acc.action)
+
+
+if __name__ == "__main__":
+    devs = get_devices()
+    print(devs)
+    for i, device in enumerate(devs):
+        turn = TurnState(1)
+        acc = AccelerateState(1)
+        # obj[device] = (turn, acc)
+        asyncio.ensure_future(print_events(device, i, turn, acc))
+
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+
+    # for dev in devs:
+    #     print(dev.capabilities())
+    #     turn = TurnState(1)
+    #     acc = AccelerateState(1)
+    #     for event in dev.read_loop():
+    #         code = event.code
+    #         type = event.type
+    #         value = event.value
+    #         type_code = (type, code)
+    #         if type_code in event_types:
+    #             event_type = event_types[type_code]
+    #             if event_type == "ABS_WHEEL":
+    #                 print(value)
+    #                 turn.update(value)
+    #             elif event_type == "ABS_GAS":
+    #                 acc.update(value)
+    #             print(turn.action, acc.action)
 
 
 
